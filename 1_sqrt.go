f5a21cfeb0f29959bf070ada6e12368613e58246
gslAgile <gsosaludu@gmail.com>
2020-03-22 18:51:45 -0300

    Resolucion y comentarios de ejercicio 1, raiz de Newton.

--- 1_sqrt.go
+++ 1_sqrt.go
@@ -1,3 +1,24 @@
+/*---------------------------------------------------------------------------------------------------------------*
+ * Ejercicio: Bucles y funciones
+ * Para jugar un poco con funciones y bucles, implementa la raíz cuadrada usando el método de Newton.
+ *
+ * En este caso, el método de Newton consiste en aproximar Sqrt(x), escogiendo un punto inicial z y luego repitiendo:
+ *						z = z - [(z^2 - x)/(2*z)]
+ *
+ * Newton's method
+ * Para empezar, repite el cálculo 10 veces y mira cuánto te aproximas al valor real para diferentes valores
+ * (1, 2, 3, ...).
+ *
+ * Luego, cambia la condición del bucle para que se detenga cuando el valor no cambie (o cambie menos que un valor 
+ * delta muy pequeño).
+ * Comprueba si eso da más o menos iteraciones. Cuánto te aproximas a la función math.Sqrt?
+ * 
+ * Pista: para declarar e inicializar un valor real, utiliza un literal con sintaxis de número real o utiliza una
+ * conversión:
+ *
+ *	z := float64(1)
+ *	z := 1.0
+ *---------------------------------------------------------------------------------------------------------------*/
 package main
 
 import (
